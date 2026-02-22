96d151a719b5f6f0a98a60d2c8dcfaf65abc4597
Tyler B <bremont@usf.edu>
2026-02-22 10:35:11 -0500

    Created array of employee records

---
+++ cc_5.js
@@ -0,0 +1,9 @@
+// employee records
+const employees = [
+  { name: "Ava", hourlyRate: 18.5, hoursWorked: 38 },
+  { name: "Marcus", hourlyRate: 22.0, hoursWorked: 45 },
+  { name: "Sofia", hourlyRate: 16.75, hoursWorked: 52 },
+  { name: "Tyler", hourlyRate: 20.0, hoursWorked: 40 },
+];
+
+console.log("Loaded employees:", employees);
